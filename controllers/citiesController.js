c22ea335b3750983427e4244754caf03a827233b
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-03-08 17:32:49 -0300

    middleware cors + middleware express.json

---
+++ controllers/citiesController.js
@@ -0,0 +1,14 @@
+import City from "../models/City";
+
+const citiesController = {
+    // getAllCities: async (req, res, next) => {
+    //     let cities;
+    //     let error = null;
+    //     let success = true;
+    //     try {
+    //         cities = await City.find().populate({
+    //             path: 
+    //         })
+    //     }
+    // }
+}
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import './config/database.js';
 
 const server = express();
 
+server.use(cors());
+server.use(express.json());
+
 server.use('/', indexRouter)
 
 server.listen(process.env['PORT'], ()=>{console.log("server running on http://localhost:"+process.env['PORT'])});
