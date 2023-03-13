90ade2af7eea8f56b0a156fde8a52da6e0732f61
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-03-13 17:32:49 -0300

    install dotenv and cors libraries

---
+++ routers/citiesRouter.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const citiesRouter = Router();
+
+// citiesRouter.get('/', getAllCities);
+// citiesRouter.get('/:id', getOneCityById);
+
+export default citiesRouter;
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import citiesRouter from "./citiesRouter.js";
 
 const indexRouter = Router();
 
 indexRouter.get('/', (request, response, next) => {
     response.send("welcome to the API of MyTinerary");
 })
+
+indexRouter.use('/cities', citiesRouter)
 
 export default indexRouter;
